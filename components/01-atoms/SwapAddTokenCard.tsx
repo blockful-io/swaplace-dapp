153cda2b59f75d05cfbff14e978e121dc664432c
= <=>
2024-02-24 09:33:26 -0300

    feat: Add Token Manually Atom/Button

--- components/01-atoms/SwapAddTokenCard.tsx
+++ components/01-atoms/SwapAddTokenCard.tsx
@@ -1,37 +1,7 @@
 import { PlusIcon } from "./icons/PlusIcon";
-import { SwapAddManuallyModalLayout } from "@/components/01-atoms";
+import { SwapAddManuallyModalLayout, Tooltip } from "@/components/01-atoms";
 import { useState } from "react";
-
-const Tooltip = ({ message }: { message: string }) => {
-  return (
-    <div
-      className="absolute 
-    left-1/2 top-[-40px] 
-    ml-auto mr-auto min-w-max 
-    -translate-x-1/2 scale-0 
-    transform rounded-lg 
-    px-3 py-2 text-xs 
-    font-medium transition-all 
-    duration-500 group-hover:scale-100"
-    >
-      <div className="flex max-w-xs flex-col items-center ">
-        <div className="rounded bg-gray-800 p-2 text-center text-xs text-white shadow-lg">
-          {message}
-        </div>
-        <div
-          className="
-            w-0 h-0 
-            border-l-[8px] border-l-transparent
-            border-t-[12px] border-t-gray-800
-            border-r-[8px] border-r-transparent 
-            shadow-lg"
-        ></div>
-      </div>
-    </div>
-  );
-};
-
-export const AddTokenCard = () => {
+export const SwapAddTokenCard = () => {
   const [open, setOpen] = useState(false);
 
   return (
@@ -50,7 +20,7 @@ export const AddTokenCard = () => {
           transition-all duration-200
         "
         >
-        <Tooltip message="Add Tokens" />
+        <Tooltip content="Add Token" position="top" />
         <div className="flex items-center justify-center h-full">
           <button
             onClick={() => setOpen(!open)}
--- components/02-molecules/NftsList.tsx
+++ components/02-molecules/NftsList.tsx
@@ -1,11 +1,12 @@
 import { NFT } from "@/lib/client/constants";
 import { NftCard } from "@/components/02-molecules";
-import { EmptyNftsCards, AddTokenCard } from "@/components/01-atoms";
+import { EmptyNftsCards, SwapAddTokenCard } from "@/components/01-atoms";
 
 /* eslint-disable react/jsx-key */
 interface INftsList {
   nftsList: NFT[];
   ownerAddress: string | null;
+  variant: "your" | "their";
 }
 
 /**
@@ -17,20 +18,28 @@ interface INftsList {
  * @returns NftsList
  */
 
-export const NftsList = ({ nftsList, ownerAddress }: INftsList) => {
-  const emptySquares = EmptyNftsCards(nftsList.length+1, 15, 30, 30, 30);
-  const addTokenSquare = AddTokenCard();
+export const NftsList = ({ nftsList, ownerAddress, variant }: INftsList) => {
+  const emptySquares = EmptyNftsCards(nftsList.length + 1, 15, 30, 30, 30);
+  const addTokenSquare = SwapAddTokenCard();
   const nftSquares = nftsList.map((nft: NFT, index) => (
     <div key={`nft-${index}`}>
       <NftCard ownerAddress={ownerAddress} nftData={nft} />
     </div>
   ));
 
-  const allSquares = [...nftSquares, addTokenSquare, ...emptySquares];
-
-  return (
-    <div className="w-full grid grid-cols-3 md:grid-cols-6 lg:grid-cols-6 gap-3 py-6 px-4">
-      {allSquares}
-    </div>
-  );
+  if (variant === "your") {
+    const allSquares = [...nftSquares, addTokenSquare, ...emptySquares];
+    return (
+      <div className="w-full grid grid-cols-3 md:grid-cols-6 lg:grid-cols-6 gap-3 py-6 px-4">
+        {allSquares}
+      </div>
+    );
+  } else {
+    const allSquares = [...emptySquares, ...nftSquares];
+    return (
+      <div className="w-full grid grid-cols-3 md:grid-cols-6 lg:grid-cols-6 gap-3 py-6 px-4">
+        {allSquares}
+      </div>
+    );
+  }
 };
