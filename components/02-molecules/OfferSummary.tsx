ac08c690527f16b4baa78ee52d510aa1281907bc
Gabriel José <biel.jose@hotmail.com>
2024-02-27 14:36:55 -0300

    feat/when the address has a user name, the OfferSummary shows the username instead the address.

--- components/02-molecules/OfferSummary.tsx
+++ components/02-molecules/OfferSummary.tsx
@@ -2,7 +2,9 @@ import { EthereumAddress } from "@/lib/shared/types";
 import { useAuthenticatedUser } from "@/lib/client/hooks/useAuthenticatedUser";
 import { NftCard } from "@/components/02-molecules";
 import { ENSAvatar, EmptyNftsCards, PersonIcon, SwapContext } from "@/components/01-atoms";
-import { useContext } from "react";
+import { useEnsData } from "@/lib/client/hooks/useENSData";
+import { useContext, useEffect } from "react";
+
 
 interface IOfferSummary {
   forAuthedUser: boolean;
@@ -24,13 +26,24 @@ export const OfferSummary = ({ forAuthedUser }: IOfferSummary) => {
 
   const nftUser = forAuthedUser ? nftAuthUser : nftInputUser;
 
+  const { primaryName: name1 } = useEnsData({
+    ensAddress: authenticatedUserAddress
+  })
+  const { primaryName: name2 } = useEnsData({
+    ensAddress: validatedAddressToSwap as unknown as EthereumAddress
+  })
+
+  useEffect(()=>{
+    console.log(new EthereumAddress(validatedAddressToSwap))
+  },[validatedAddressToSwap])
+  
   return (
     <div className="w-full flex flex-col gap-4 px-3 pt-2 pb-4 dark:bg-[#212322] dark:border-[#434443] rounded-lg border">
       <div className="flex justify-between items-center h-9 gap-2">
         <div className="flex space-x-2">
           <div className="flex items-center">
             {!forAuthedUser && authenticatedUserAddress ?
-              <ENSAvatar avatarENSAddress={authenticatedUserAddress} size="small"/>
+              <ENSAvatar avatarENSAddress={authenticatedUserAddress} size="small" />
               : forAuthedUser && validatedAddressToSwap ?
                 <ENSAvatar avatarENSAddress={new EthereumAddress(validatedAddressToSwap)} size="small" />
                 : <PersonIcon />}
@@ -38,13 +51,17 @@ export const OfferSummary = ({ forAuthedUser }: IOfferSummary) => {
           <div className="items-center">
             <p className="font-medium">
               {
-                !forAuthedUser && authenticatedUserAddress ?
-                  `${new EthereumAddress(authenticatedUserAddress.address).getEllipsedAddress()} gets` :
-                  !forAuthedUser ? `You Get` :
-                    forAuthedUser && validatedAddressToSwap && inputAddress ?
-                      `${new EthereumAddress(validatedAddressToSwap).getEllipsedAddress()} gets` :
-                      forAuthedUser && !validatedAddressToSwap || !inputAddress ?
-                        `Use the search bar` : `Use the search bar`
+                !forAuthedUser && authenticatedUserAddress && name1 ?
+                  `${name1} gets` :
+                  !forAuthedUser && authenticatedUserAddress ?
+                    `${new EthereumAddress(authenticatedUserAddress.address).getEllipsedAddress()} gets` :
+                    !forAuthedUser ? `You Get` :
+                      forAuthedUser && validatedAddressToSwap && inputAddress && name2 ?
+                        `${name2} gets` :
+                        forAuthedUser && validatedAddressToSwap && inputAddress
+                          ? `${new EthereumAddress(validatedAddressToSwap).getEllipsedAddress()} gets` :
+                          forAuthedUser && !validatedAddressToSwap || !inputAddress ?
+                            `Use the search bar` : `Use the search bar`
               }
             </p>
           </div>
