64c1482d7332151340bb724c95bbd887d7ebf931
eduramme <eduramme@gmail.com>
2024-02-28 16:17:40 -0300

    use connectedChain hook inside Layout component

--- components/04-templates/Layout.tsx
+++ components/04-templates/Layout.tsx
@@ -1,9 +1,11 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { SidebarProvider } from "@/lib/client/contexts/SidebarContext.tsx";
 import { useAuthedAccess } from "@/lib/client/hooks/useAuthedAccess";
+import { useConnectedChain } from "@/lib/client/hooks/useConnectedChain";
 
 export const Layout = ({ children }: { children: React.ReactNode }) => {
   useAuthedAccess();
+  useConnectedChain();
 
   return (
     <SidebarProvider>
