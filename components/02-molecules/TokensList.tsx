f93d445c036382dbb305fdc37bee4d2607f610f8
frankind.eth <faguzzi3@gmail.com>
2024-02-28 20:49:33 -0300

    feat: Merged develop updates

--- components/02-molecules/TokensList.tsx
+++ components/02-molecules/TokensList.tsx
@@ -1,10 +1,11 @@
 import { TokenCard } from "@/components/02-molecules";
-import { TokenCardsPlaceholder } from "@/components/01-atoms";
+import { SwapAddTokenCard, TokenCardsPlaceholder } from "@/components/01-atoms";
 import { Token } from "@/lib/shared/types";
 
 export interface TokensListProps {
   tokensList: Token[];
   ownerAddress: string | null;
+  variant: "your" | "their";
 }
 
 /**
@@ -16,7 +17,11 @@ export interface TokensListProps {
  * @returns TokensList
  */
 
-export const TokensList = ({ tokensList, ownerAddress }: TokensListProps) => {
+export const TokensList = ({
+  tokensList,
+  ownerAddress,
+  variant,
+}: TokensListProps) => {
   const placeholders = TokenCardsPlaceholder(tokensList.length, 15, 30, 30, 30);
   const tokenCards = tokensList.map((token: Token, index) => (
     <div key={`nft-${index}`}>
@@ -24,11 +29,23 @@ export const TokensList = ({ tokensList, ownerAddress }: TokensListProps) => {
     </div>
   ));
 
-  const allSquares = [...tokenCards, ...placeholders];
+  let allSquares = [...tokenCards, ...placeholders];
 
-  return (
-    <div className="w-full grid grid-cols-3 md:grid-cols-6 lg:grid-cols-6 gap-3 py-6 px-4">
-      {allSquares}
-    </div>
-  );
+  const addTokenSquare = SwapAddTokenCard();
+
+  if (variant === "your") {
+    placeholders.pop(); // Removes the last element to fill with addToken
+    allSquares = [...allSquares, addTokenSquare];
+    return (
+      <div className="w-full grid grid-cols-3 md:grid-cols-6 lg:grid-cols-6 gap-3 py-6 px-4">
+        {allSquares}
+      </div>
+    );
+  } else {
+    return (
+      <div className="w-full grid grid-cols-3 md:grid-cols-6 lg:grid-cols-6 gap-3 py-6 px-4">
+        {allSquares}
+      </div>
+    );
+  }
 };
