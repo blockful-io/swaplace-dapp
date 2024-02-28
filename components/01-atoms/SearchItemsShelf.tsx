b9f89b1960971b20ba8a838de583bb14a28a5728
heronlancellot <heronlancellot@id.uff.br>
2024-02-27 22:14:42 -0300

    update: index & searchItemsShelf max-width

--- components/01-atoms/SearchItemsShelf.tsx
+++ components/01-atoms/SearchItemsShelf.tsx
@@ -1,4 +1,4 @@
-import { MagnifyingGlassIcon } from "@/components/01-atoms/icons/MagnifyingGlassIcon";
+import { MagnifyingGlassIcon } from "@/components/01-atoms";
 import { useTheme } from "next-themes";
 import cc from "classcat";
 
@@ -12,7 +12,7 @@ export const SearchItemsShelf = () => {
         name="search"
         type="search"
         className={cc([
-          "flex items-center pl-9 rounded-lg h-8 gap-3 font-normal leading-5 text-sm not-italic placeholder-[#707572] border border-[#353836] opacity-80 bg-[#282B29] transition duration-300 ease-in-out focus:outline-none",
+          "flex items-center pl-9 rounded-lg max-w-[216px] h-8 gap-3 font-normal leading-5 text-sm not-italic placeholder-[#707572] border border-[#353836] opacity-80 bg-[#282B29] transition duration-300 ease-in-out focus:outline-none",
           theme == "light"
             ? "bg-[#F6F6F6] hover:bg-[#F0EEEE] border-[#E4E4E4] hover:border-[#D6D5D5] focus:border-[#AABE13] hover:shadow-[0_0_6px_1px_#00000014] focus:shadow-[0_0_6px_1px_#00000014] focus:border-opacity-100"
             : "hover:dark:bg-[#353836] hover:dark:border-[#505150] hover:dark:shadow-[0_0_6px_1px_#00000066] focus:dark:shadow-[0_0_6px_1px_#00000066] focus:dark:border-[#AABE13]",
