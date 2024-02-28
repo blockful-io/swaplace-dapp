c5ddafe2c04e390f348aa0cf15831344295644dc
eduramme <eduramme@gmail.com>
2024-02-28 16:09:23 -0300

    create snackbar remove button

--- lib/client/hooks/useConnectedChain.tsx
+++ lib/client/hooks/useConnectedChain.tsx
@@ -1,6 +1,7 @@
 import { ChainInfo } from "../constants";
 import { useEffect, useState } from "react";
-import {  useNetwork } from "wagmi";
+import toast from "react-hot-toast";
+import {  sepolia, useNetwork, useSwitchNetwork } from "wagmi";
 
 export const useConnectedChain = () => {
   const [isNetworkSupported, setIsNetworkSupported] = useState(true);
@@ -9,13 +10,19 @@ export const useConnectedChain = () => {
 
   const { chain } = useNetwork();
 
+  const { switchNetwork } = useSwitchNetwork();
+
   useEffect(() => {
     if (chain && supportedNetworksId.includes(chain.id)) {
       setIsNetworkSupported(true);
     } else {
       setIsNetworkSupported(false);
+      toast.error(`Network not supported, change network and try again`, {
+        id: "welcome-toast",
+      });
+      switchNetwork && switchNetwork(sepolia.id)
     }
-  }, [chain, supportedNetworksId]);
+  }, [chain, supportedNetworksId, switchNetwork]);
 
   return {
     isNetworkSupported,
