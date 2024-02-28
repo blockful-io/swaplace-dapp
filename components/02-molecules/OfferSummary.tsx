fd233e94721747e4164f382307baca9cd4cc2e22
heronlancellot <heronlancellot@id.uff.br>
2024-02-28 20:11:36 -0300

    feat: add new style in dApp in all devices

--- components/02-molecules/OfferSummary.tsx
+++ components/02-molecules/OfferSummary.tsx
@@ -18,76 +18,87 @@ export const OfferSummary = ({ forAuthedUser }: IOfferSummary) => {
   } = useContext(SwapContext);
 
   const { authenticatedUserAddress } = useAuthenticatedUser();
-  const emptySquaresDefault = EmptyNftsCards(0, 4, 8, 12, 12);
-  const emptySquaresAuthUser = EmptyNftsCards(nftAuthUser.length, 4, 8, 12, 12);
-  const emptySquaresInputUser = EmptyNftsCards(
-    nftInputUser.length,
-    4,
+  const emptySquaresDefault = EmptyNftsCards(0, 6, 10, 8, 12, "medium");
+  const emptySquaresAuthUser = EmptyNftsCards(
+    nftAuthUser.length,
+    6,
+    10,
     8,
     12,
+    "medium",
+  );
+  const emptySquaresInputUser = EmptyNftsCards(
+    nftInputUser.length,
+    6,
+    10,
+    8,
     12,
+    "medium",
   );
 
   const nftUser = forAuthedUser ? nftAuthUser : nftInputUser;
 
   return (
-    <div className="w-full flex flex-col gap-4 px-3 pt-2 pb-4 dark:bg-[#212322] dark:border-[#434443] rounded-lg border">
-      <div className="flex justify-between items-center h-9 gap-2">
-        <div className="flex space-x-2">
-          <div className="flex items-center">
-            <PersonIcon />
+    <div className="w-full h-full dark:bg-[#282B29] border dark:border-[#353836] rounded-lg ">
+      <div className="flex flex-col gap-4 px-4 pt-2 pb-4">
+        <div className="flex justify-between items-center h-9 gap-2">
+          <div className="flex space-x-2">
+            <div className="flex items-center">
+              <PersonIcon />
+            </div>
+            <div className="items-center">
+              <p className="font-medium">
+                {forAuthedUser
+                  ? "You give"
+                  : !forAuthedUser && validatedAddressToSwap && inputAddress
+                  ? `${
+                      userJustValidatedInput
+                        ? new EthereumAddress(
+                            validatedAddressToSwap,
+                          ).getEllipsedAddress() + " gives"
+                        : "Use the search bar!"
+                    }`
+                  : "Use the search bar!"}
+              </p>
+            </div>
           </div>
-          <div className="items-center">
-            <p className="font-medium">
-              {forAuthedUser
-                ? "You give"
-                : !forAuthedUser && validatedAddressToSwap && inputAddress
-                ? `${
-                    userJustValidatedInput
-                      ? new EthereumAddress(
-                          validatedAddressToSwap,
-                        ).getEllipsedAddress() + " gives"
-                      : "Use the search bar!"
-                  }`
-                : "Use the search bar!"}
-            </p>
-          </div>
-        </div>
-        {!forAuthedUser && !validatedAddressToSwap ? null : (
-          <div>
-            {nftUser.length} item
-            {nftUser.length !== 1 ? "s" : ""}
-          </div>
-        )}
-      </div>
-
-      <div className="w-full h-full min-h-[144px] rounded p-4 overflow-auto max-h-52 no-scrollbar">
-        <div className="w-full grid grid-cols-2 md:grid-cols-6  xl:grid-cols-4 gap-3 ">
-          {(forAuthedUser && !authenticatedUserAddress?.address) ||
-          (!forAuthedUser && !validatedAddressToSwap) ? null : (
-            <>
-              {nftUser.map((nft, index) => (
-                <NftCard
-                  key={index}
-                  withSelectionValidation={false}
-                  ownerAddress={
-                    forAuthedUser
-                      ? authenticatedUserAddress
-                        ? authenticatedUserAddress.address
-                        : null
-                      : validatedAddressToSwap
-                  }
-                  nftData={nft}
-                />
-              ))}
-            </>
+          {!forAuthedUser && !validatedAddressToSwap ? null : (
+            <div>
+              {nftUser.length} item
+              {nftUser.length !== 1 ? "s" : ""}
+            </div>
           )}
+        </div>
 
-          {forAuthedUser
-            ? emptySquaresAuthUser
-            : !forAuthedUser
-            ? emptySquaresInputUser
-            : emptySquaresDefault}
+        <div className="w-full h-full max-h-[156px] rounded overflow-auto no-scrollbar">
+          <div className="w-full grid grid-cols-3 md:grid-cols-6 xl:grid-cols-5 lg:grid-cols-4 gap-3  ">
+            {(forAuthedUser && !authenticatedUserAddress?.address) ||
+            (!forAuthedUser && !validatedAddressToSwap) ? null : (
+              <>
+                {nftUser.map((nft, index) => (
+                  <NftCard
+                    key={index}
+                    withSelectionValidation={false}
+                    ownerAddress={
+                      forAuthedUser
+                        ? authenticatedUserAddress
+                          ? authenticatedUserAddress.address
+                          : null
+                        : validatedAddressToSwap
+                    }
+                    nftData={nft}
+                    styleType="medium"
+                  />
+                ))}
+              </>
+            )}
+
+            {forAuthedUser
+              ? emptySquaresAuthUser
+              : !forAuthedUser
+              ? emptySquaresInputUser
+              : emptySquaresDefault}
+          </div>
         </div>
       </div>
     </div>
--- components/04-templates/SwapSection.tsx
+++ components/04-templates/SwapSection.tsx
@@ -4,15 +4,15 @@ import { SwapStation, SwappingShelfs } from "@/components/03-organisms";
 
 export const SwapSection = () => {
   return (
-    <div className="xl:w-[1280px] xl:h-[720px]  w-full flex flex-col justify-center xl:flex-row">
+    <div className="max-w-[1280px] max-h-[720px] w-full flex xl:flex-row flex-col lg:justify-center h-full">
       <TheHeader />
-      <section className="flex items-center xl:px-[60px] xl:py-[32px]">
-        <div className="xl:h-[656px] bg-red-500 xl:w-[1098px] flex xl:gap-[20px]">
-          <div className="flex flex-col xl:w-[600px] bg-yellow-500 ">
+      <section className="flex items-center xl:px-[60px] xl:py-[32px] xl:flex-row flex-col">
+        <div className="flex lg:flex-row flex-col xl:h-[656px] xl:w-[1098px] lg:gap-[20px] lg:mt-5 w-[95%] gap-2">
+          <div className="flex flex-col lg:w-[600px]  ">
             <SearchBar />
             <SwappingShelfs />
           </div>
-          <div className="flex xl:w-[438px] ">
+          <div className="flex xl:w-[478px] ">
             <SwapStation />
           </div>
         </div>
