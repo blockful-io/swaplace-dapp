d0e84c9315de00ec5ab3b38e019c49a3cb0bbce0
heronlancellot <heronlancellot@id.uff.br>
2024-02-29 11:51:17 -0300

    fix: offers page in all devices & adjust the page like swap-section

--- components/04-templates/OfferSection.tsx
+++ components/04-templates/OfferSection.tsx
@@ -1,15 +1,20 @@
 import { TokenOffers } from "@/components/03-organisms";
-import { FilterOffers } from "@/components/02-molecules";
+import { FilterOffers, TheHeader } from "@/components/02-molecules";
 
 export const OfferSection = () => {
   return (
-    <section className="w-full h-full flex xl:flex-row flex-col items-center justify-center ">
-      <div className="flex">
-        <FilterOffers />
-      </div>
-      <div className="flex flex-col justify-center items-center">
-        <TokenOffers />
-      </div>
-    </section>
+    <div className="max-w-[1280px] max-h-[720px] w-full flex xl:flex-row flex-col lg:justify-center h-full">
+      <TheHeader />
+      <section className="flex items-center  xl:px-[60px] xl:pt-[32px] xl:flex-row flex-col">
+        <div className="flex xl:flex-row flex-col h-full xl:w-[1098px] w-[95%] justify-between xl:items-start md:items-center">
+          <div className="flex flex-col">
+            <FilterOffers />
+          </div>
+          <div className="flex xl:flex-col ">
+            <TokenOffers />
+          </div>
+        </div>
+      </section>
+    </div>
   );
 };
--- pages/offers.tsx
+++ pages/offers.tsx
@@ -1,26 +1,17 @@
-import { TheHeader } from "@/components/02-molecules";
 import { Layout } from "@/components/04-templates";
 import { OfferSection } from "@/components/04-templates/OfferSection";
-import { useAuthenticatedUser } from "@/lib/client/hooks/useAuthenticatedUser";
 import cc from "classcat";
 
 export default function Offers() {
-  const { authenticatedUserAddress } = useAuthenticatedUser();
-
   return (
     <Layout>
-      {authenticatedUserAddress && (
-        <div
-          className={cc([
-            "border xl:w-auto w-full xl:mt-16 mt-12 xl:h-full space-y-6 flex xl:flex-row flex-col justify-items-stretch items-center mx-1",
-          ])}
-        >
-          <div className="xl:h-[656px] xl:w-[62px] w-full flex justify-center mr-4 ml-2">
-          <TheHeader />
-          </div>
-          <OfferSection />
-        </div>
-      )}
+      <div
+        className={cc([
+          "w-screen h-screen flex flex-col justify-center items-center",
+        ])}
+      >
+        <OfferSection />
+      </div>
     </Layout>
   );
 }
