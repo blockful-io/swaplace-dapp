ce0f0ff4d928ea76458c2f9e86e0602e3c18fb8b
heronlancellot <heronlancellot@id.uff.br>
2024-03-04 17:09:01 -0300

    feat: add swap-connection organism in swap-section

--- components/04-templates/SwapSection.tsx
+++ components/04-templates/SwapSection.tsx
@@ -1,15 +1,18 @@
-import { SearchBar } from "@/components/01-atoms";
 import { TheHeader } from "@/components/02-molecules";
-import { SwapStation, SwappingShelfs } from "@/components/03-organisms";
+import {
+  SwapConnection,
+  SwapStation,
+  SwappingShelfs,
+} from "@/components/03-organisms";
 
 export const SwapSection = () => {
   return (
     <div className="max-w-[1280px] max-h-[720px] w-full flex xl:flex-row flex-col lg:justify-center h-full">
       <TheHeader />
       <section className="flex items-center xl:px-[60px] xl:py-[32px] xl:flex-row flex-col">
-        <div className="flex lg:flex-row flex-col xl:h-[656px] xl:w-[1098px] lg:gap-[20px] lg:mt-5 w-[95%] gap-2">
-          <div className="flex flex-col lg:w-[600px]  ">
-            <SearchBar />
+        <div className="flex lg:flex-row flex-col xl:h-[656px] xl:w-[1098px] lg:gap-[20px] w-[95%] gap-2">
+          <div className="flex flex-col lg:w-[600px] gap-8">
+            <SwapConnection />
             <SwappingShelfs />
           </div>
           <div className="flex xl:w-[478px] ">
