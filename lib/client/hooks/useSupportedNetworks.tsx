e9887c9937c8d97526d270616a232d3cbf2aadb0
heronlancellot <heronlancellot@id.uff.br>
2024-03-04 16:47:17 -0300

    refator: remove polygon & hardhat & add only sepolia to supported-networks hook for now

--- lib/client/hooks/useSupportedNetworks.tsx
+++ lib/client/hooks/useSupportedNetworks.tsx
@@ -1,16 +1,18 @@
 import { ChainInfo } from "../constants";
 import { useEffect, useState } from "react";
-import {  useNetwork } from "wagmi";
+import { useNetwork } from "wagmi";
 
 export const useSupportedNetworks = () => {
   const [isNetworkSupported, setIsNetworkSupported] = useState(true);
-
-  const supportedNetworksId = Object.values(ChainInfo).map(net => net.id)
-
+  const supportedNetworksId = Object.values(ChainInfo).map((net) => net.id);
   const { chain } = useNetwork();
 
   useEffect(() => {
-    if (chain && supportedNetworksId.includes(chain.id)) {
+    if (
+      chain &&
+      supportedNetworksId.includes(chain.id) &&
+      chain.id === 11155111 // Hardcoded for now to accept only Sepolia for now, until we accept others chains afther alpha
+    ) {
       setIsNetworkSupported(true);
     } else {
       setIsNetworkSupported(false);
