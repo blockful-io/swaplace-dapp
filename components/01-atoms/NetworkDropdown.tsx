8e171ac9dad94f83a6d104b8fe304b1bc78e43c0
0xneves <asguilift@hotmail.com>
2024-03-06 08:56:40 +0700

    fix: networkDropdown with wrong border color

--- components/01-atoms/NetworkDropdown.tsx
+++ components/01-atoms/NetworkDropdown.tsx
@@ -127,8 +127,8 @@ export const NetworkDropdown = ({ forAuthedUser }: NetworkDropdownProps) => {
       >
         <div
           className={cc([
-            "lg:max-w-[280px] max-h-[44px] rounded-xl pl-3 pr-4 dark:bg-[#212322] bg-[#F6F6F6] border dark:border-[#353836] border-[#D6D5D5] hover:dark:border-[#505150] transition duration-300 ease-in-out dark:shadow-swap-connection shadow-swap-connection-light",
-            isOpen && "dark:border-[#DDF23D33] hover:dark:border-[#DDF23D33]",
+            "lg:max-w-[280px] max-h-[44px] rounded-xl pl-3 pr-4 dark:bg-[#212322] bg-[#F6F6F6] border dark:border-[#353836] border-[#D6D5D5] transition duration-300 ease-in-out dark:shadow-swap-connection shadow-swap-connection-light",
+            isOpen && "dark:border-[#DDF23D33] hover:dark:border-[#edff6259]",
           ])}
         >
           <div className="flex justify-between items-center h-[44px] lg:w-[252px]">
