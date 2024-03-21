f81b779405c29968c4f925cdfb8357fd7c7f2d92
Heron Lancellot <heronlancellot@Herons-Laptop.localdomain>
2024-03-21 13:53:33 -0300

    fix: adjust swap-offer-layout to better types

--- components/02-molecules/SwapOffersLayout.tsx
+++ components/02-molecules/SwapOffersLayout.tsx
@@ -15,37 +15,17 @@ interface EmptyLayoutOffersProps {
   button: React.ReactNode;
 }
 
-const EmptyLayoutOffers = ({
-  icon,
-  title,
-  description,
-  button,
-}: EmptyLayoutOffersProps) => {
-  return (
-    <div className="md:w-[676px] md:h-[656px] w-[95%] h-full py-10 px-5 border border-[#353836] dark:bg-[#212322] bg-[#F6F6F6] rounded-lg flex flex-col justify-center items-center gap-5">
-      <div className="flex ">{icon}</div>
-      <div className="flex flex-col text-center items-center">
-        <p className="dark:p-medium-bold-2-dark p-medium-bold-2-dark-variant-black ">
-          {title}
-        </p>
-        <p className="p-small dark:!text-[#A3A9A5] !text-[#212322] ">
-          {description}
-        </p>
-      </div>
-      <div className="flex">{button}</div>
-    </div>
-  );
-};
-
 export enum SwapOffersDisplayVariant {
   ERROR = "error",
   NO_SWAPS_CREATED = "swapless",
 }
 
-type Variant = SwapOffersDisplayVariant | "error" | "swapless";
-
 interface SwapOffersLayoutProps {
-  variant: Variant;
+  variant: SwapOffersDisplayVariant;
+}
+
+interface SwapOffersConfig {
+  body: React.ReactNode;
 }
 
 export const SwapOffersLayout = ({ variant }: SwapOffersLayoutProps) => {
@@ -53,9 +33,31 @@ export const SwapOffersLayout = ({ variant }: SwapOffersLayoutProps) => {
   const [toggleManually, setToggleManually] = useState<boolean>(false);
   const router = useRouter();
 
-  return (
-    <>
-      {variant === SwapOffersDisplayVariant.ERROR ? (
+  const EmptyLayoutOffers = ({
+    icon,
+    title,
+    description,
+    button,
+  }: EmptyLayoutOffersProps) => {
+    return (
+      <div className="md:w-[676px] md:h-[656px] w-[95%] h-full py-10 px-5 border border-[#353836] dark:bg-[#212322] bg-[#F6F6F6] rounded-lg flex flex-col justify-center items-center gap-5">
+        <div className="flex ">{icon}</div>
+        <div className="flex flex-col text-center items-center">
+          <p className="dark:p-medium-bold-2-dark p-medium-bold-2-dark-variant-black ">
+            {title}
+          </p>
+          <p className="p-small dark:!text-[#A3A9A5] !text-[#212322] ">
+            {description}
+          </p>
+        </div>
+        <div className="flex">{button}</div>
+      </div>
+    );
+  };
+
+  const SwapOffersLayoutError = () => {
+    return (
+      <>
         <EmptyLayoutOffers
           icon={<ErrorIcon fill={cc([{ black: theme === "light" }])} />}
           title={<> Sorry, we couldn&apos;t load your swaps</>}
@@ -78,7 +80,13 @@ export const SwapOffersLayout = ({ variant }: SwapOffersLayoutProps) => {
             </>
           }
         />
-      ) : (
+      </>
+    );
+  };
+
+  const SwapOffersLayoutNoSwapsCreated = () => {
+    return (
+      <>
         <EmptyLayoutOffers
           icon={<NoSwapsIcon fill={cc([{ black: theme === "light" }])} />}
           title={<> No swaps here. Let&apos;s fill it up!</>}
@@ -97,7 +105,21 @@ export const SwapOffersLayout = ({ variant }: SwapOffersLayoutProps) => {
             </button>
           }
         />
-      )}
-    </>
-  );
+      </>
+    );
+  };
+
+  const SwapOffersConfigLayout: Record<
+    SwapOffersDisplayVariant,
+    SwapOffersConfig
+  > = {
+    [SwapOffersDisplayVariant.ERROR]: {
+      body: <SwapOffersLayoutError />,
+    },
+    [SwapOffersDisplayVariant.NO_SWAPS_CREATED]: {
+      body: <SwapOffersLayoutNoSwapsCreated />,
+    },
+  };
+
+  return <>{SwapOffersConfigLayout[variant].body}</>;
 };
